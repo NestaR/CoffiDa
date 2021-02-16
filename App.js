25799970340c918b346cf7edbef82e9270b16b48
Nesta Rubio <nestarubio1@gmail.com>
2021-02-16 16:24:26 +0000

    Change main screen

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from './components/login';
-import About from './components/about';
+import Login from './components/login';
+import Home from './components/home';
 import Contact from './components/contact';
 
 const Stack = createStackNavigator();
@@ -16,8 +16,8 @@ class App extends Component{
     return(
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Log In" component={Home} />
-          <Stack.Screen name="About" component={About} />
+          <Stack.Screen name="Log In" component={Login} />
+          <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Contact" component={Contact} />
         </Stack.Navigator>
       </NavigationContainer>
